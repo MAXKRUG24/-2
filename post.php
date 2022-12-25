7bc4b88f25d2d1f3d087442df62d33a913968fc1
MAXKRUG24 <90874160+MAXKRUG24@users.noreply.github.com>
2022-12-25 16:49:14 +0300

    Update post.php

--- post.php
+++ post.php
@@ -2,7 +2,7 @@
     require_once 'connect.php';
 
     $post = $_POST['post'];
-    $login = $_POST['login'];
+    $login = $_POST['log'];
     $date = date(" H : i : s d - m - Y ");
     $rwr = mysqli_query($connect, "INSERT INTO `post`(`id`, `login`, `text`, `dtime`) VALUES(NULL, '$login', '".addslashes($post)."', '$date')");
     //mysqli_query($connect, "INSERT INTO `users` (`id`, `login`, `email`, `password`,`secret_word`) VALUES (NULL, '$login', '$email', '$password_hsh','$secret_word_hsh')");
@@ -10,5 +10,4 @@
       echo "проблемы с БД";
     }
     header('Location: index.php');
-    //echo "<script> window.location.href='index.php';</script>";
  ?>
